51180224edec35f1b97d4cd6a58dd5aef0b139e9
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-24 17:27:21 +0200

    added first try at bonus 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// const to insert the card
+// * const to insert the card
 const cardContainer = document.getElementById("card-container");
 
-// const team with all members
+// * const team with all members
 
 const team = [
   // first member
@@ -42,45 +42,44 @@ const team = [
   },
 ];
 
-// for cycle to extract every object from the array
+// * for cycle to extract every object from the array
 
 for (let teamMember of team) {
-  // for cycle to extract every object value from object
+  // * for cycle to extract every object value from object
 
+  const teamCard = [];
   for (let Attribute in teamMember) {
-    if (Attribute === "img") {
-      console.log("gotcha");
-      cardContainer.innerHTML += `
-      <img src="./img/${teamMember[Attribute]}" alt="${teamMember[Attribute]}">  
-        `;
-    } else {
-      cardContainer.innerHTML += `
-        <p> ${teamMember[Attribute]} </p>
-        `;
-    }
+    // if (Attribute === "img") {
+    //   cardContainer.innerHTML += `
+    //       <img src="./img/${teamMember[Attribute]}" alt="${teamMember[Attribute]}">
+    //       `;
+    // } else {
+    //   cardContainer.innerHTML += `
+    //         <p> ${teamMember[Attribute]} </p>
+    //         `;
+    // }
+    teamCard.push(teamMember[Attribute]);
     // console.log(teamMember[Attribute]);
   }
+
+  let card = cardGenerator(teamCard[0], teamCard[1], teamCard[2]);
+  cardContainer.append(card);
 }
 
-// ! trying out a function to create cards
+// * function to create cards
 
-// generate();
+function cardGenerator(name, role, img) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML += `
+  <img class="card-img-top" src="./img/${img}" alt="${img}">
+  <div class="card-body">
+  
+  <h5 class="card-title">${name}</h5>
+  <p class="card-text">${role}</p>
+</div>
 
-// function generate() {
-//   for (let i = 0; i < team.length; i++) {
-//     let card = cardGenerator();
-//     cardContainer.append(card);
-//   }
-// }
+  `;
 
-// function cardGenerator() {
-//   const card = document.createElement("div");
-//   card.classList.add("card");
-//   card.innerHTML += `
-//   <div class="card-body">
-//   <h5 class="card-title">COSE LUNGHE</h5>
-//   <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In quibusdam deleniti dicta officiis quam nam. Facilis explicabo reiciendis esse. Ipsam hic dolores expedita culpa odio dolorum tempore ipsa, aliquid provident?</p>
-// </div>
-//   `;
-//   return card;
-// }
+  return card;
+}
