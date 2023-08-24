6dbe590725a3edf72d7d1cbfc7ebc29065082967
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-24 15:54:32 +0200

    Milestone 0 reached, adding object team

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 console.log("Hello world");
+
+const team = [
+  // first member
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    img: "wayne-barnett-founder-ceo.jpg",
+  },
+  // second member
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg",
+  },
+  // third member
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    img: "walter-gordon-office-manager.jpg",
+  },
+  // fourth member
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg",
+  },
+  // fifth member
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    img: "scott-estrada-developer.jpg",
+  },
+  // sixth member
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.log(team);
